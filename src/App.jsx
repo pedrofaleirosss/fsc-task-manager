f6a8f9cfa8837ddabb107a007c975f52a2d04b3b
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-03-04 22:19:59 -0300

    feat: add Sidebar and SidebarButton components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Sidebar from './components/Sidebar';
+
 function App() {
   return (
     <div>
-      <h1 className="mt-2 p-6 text-red-500">Hello World</h1>
+      <Sidebar />
     </div>
   );
 }
