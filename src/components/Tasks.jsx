3dda0bc0fbae59d8c71a2e80e9bf3a5bc007039d
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-03-07 22:47:09 -0300

    refactor: export icons from single file

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,9 +1,11 @@
 import Button from './Button';
-import AddIcon from '../assets/icons/add.svg?react';
-import TrashIcon from '../assets/icons/trash.svg?react';
-import SunIcon from '../assets/icons/sun.svg?react';
-import CloudSunIcon from '../assets/icons/cloud-sun.svg?react';
-import MoonIcon from '../assets/icons/moon.svg?react';
+import {
+  AddIcon,
+  SunIcon,
+  CloudSunIcon,
+  TrashIcon,
+  MoonIcon,
+} from '../assets/icons';
 import TasksSeparator from './TasksSeparator';
 import { useState } from 'react';
 import TASKS from '../constants/tasks';
