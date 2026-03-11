c6b2cd96f78ae714a6d5b677375f2ead13e9bd90
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-03-11 20:32:56 -0300

    feat: add validation errors to AddTaskDialog

--- src/components/TimeSelect.jsx
+++ src/components/TimeSelect.jsx
@@ -1,6 +1,6 @@
 import InputLabel from './InputLabel';
 
-const TimeSelect = (props) => {
+const TimeSelect = ({ errorMessage, ...rest }) => {
   return (
     <div className="flex flex-col gap-1 text-left">
       <InputLabel htmlFor="time">Horário</InputLabel>
@@ -8,15 +8,19 @@ const TimeSelect = (props) => {
       <select
         id="time"
         className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[#9A9C9F]"
-        {...props}
+        {...rest}
       >
-        <option value="" selecter disabled>
+        <option value="" disabled>
           Selecione
         </option>
         <option value="morning">Manhã</option>
         <option value="afternoon">Tarde</option>
         <option value="evening">Noite</option>
       </select>
+
+      {errorMessage && (
+        <p className="text-left text-xs text-red-500">{errorMessage}</p>
+      )}
     </div>
   );
 };
