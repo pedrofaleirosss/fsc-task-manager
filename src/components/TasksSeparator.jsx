977b5131c5c41904e1c5254c11b1c70be6588ee0
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-03-15 22:03:28 -0300

    chore: add prop-types to TasksSeparator component

--- src/components/TasksSeparator.jsx
+++ src/components/TasksSeparator.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const TasksSeparator = ({ tittle, icon }) => {
   return (
     <div className="flex items-center gap-2 border-b border-solid border-[#f4f4f5] pb-1">
@@ -5,6 +7,11 @@ const TasksSeparator = ({ tittle, icon }) => {
       <p className="text-brand-text-gray text-sm font-semibold">{tittle}</p>
     </div>
   );
+};
+
+TasksSeparator.propTypes = {
+  tittle: PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
 };
 
 export default TasksSeparator;
