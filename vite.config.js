4e49af2304c40c2a4c9f0659c4ec1be602bcf78e
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-03-18 20:56:15 -0300

    fix: configure server to ignore db.json during file watching to not reload the page

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,9 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  server: {
+    watch: {
+      ignored: ['**/db.json'],
+    },
+  },
 });
