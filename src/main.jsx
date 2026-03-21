903be4fa433c82f6251664f5eaee05e0ff404d0b
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-03-20 22:24:59 -0300

    feat: add react-router-dom and initial TaskDetailsPage

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,25 @@ import './index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
 
 import App from './App.jsx';
+import TaskDetailsPage from './pages/task-details.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/task/:taskId',
+    element: <TaskDetailsPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
